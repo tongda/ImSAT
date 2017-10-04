662a70eacb858b8711ccf7200e92197d2f353d1f
Tong Damon Da <tongda@outlook.com>
2017-10-05 02:49:16 +0800

    change vgg part code to use tf.slim pre-trained model. solve the loading model too slow issue.

--- imsat/hook.py
+++ imsat/hook.py
@@ -4,10 +4,10 @@ from tensorflow.python.training.session_run_hook import SessionRunHook
 
 
 class IteratorInitializerHook(SessionRunHook):
-  def __init__(self):
+  def __init__(self, prefix):
     super(IteratorInitializerHook, self).__init__()
-    self.iterator_initializer = None
+    self.prefix = prefix
 
   def after_create_session(self, session, coord):
-    for initializer in tf.get_collection("initializer"):
+    for initializer in tf.get_collection(self.prefix + "_initializer"):
       session.run(initializer)
--- train.py
+++ train.py
@@ -4,47 +4,59 @@ import os
 import pickle
 
 import tensorflow as tf
+from tensorflow.contrib import slim
 from tensorflow.contrib.data import Dataset
-from tensorflow.python.estimator.estimator import Estimator
+from tensorflow.contrib.framework import assign_from_checkpoint_fn
 from tensorflow.contrib.learn import Experiment, RunConfig, ModeKeys
 from tensorflow.contrib.learn.python.learn import learn_runner
+from tensorflow.contrib.slim.nets import vgg
 from tensorflow.contrib.training import HParams
+from tensorflow.python.estimator.estimator import Estimator
 from tensorflow.python.estimator.model_fn import EstimatorSpec
 from tensorflow.python.ops.lookup_ops import HashTable, KeyValueTensorInitializer
 
 from imsat.hook import IteratorInitializerHook
 from imsat.model import AttendTell, create_loss
-from imsat.vggnet import Vgg19
 
 
 def model_fn(features, labels, mode, params, config):
   cap_lens = labels["index"].map(lambda t: tf.size(t))
   datasets = (features, labels["raw"], labels["index"], cap_lens)
   pad_size = ((224, 224, 3), (), (None,), ())
-  batches = Dataset.zip(datasets).shuffle(buffer_size=10 * params.batch_size) \
+  batches = Dataset.zip(datasets) \
+    .shuffle(buffer_size=10 * params.batch_size) \
     .padded_batch(params.batch_size, pad_size)
 
   image_tensor = caption_tensor = cap_idx_tensor = cap_len_tensor = None
+  scaffold = None
   if mode == ModeKeys.TRAIN:
     train_iterator = batches \
       .repeat() \
       .make_initializable_iterator()
     image_tensor, caption_tensor, cap_idx_tensor, cap_len_tensor = \
       train_iterator.get_next()
-    tf.add_to_collection("initializer", train_iterator.initializer)
+    tf.add_to_collection("train_initializer", train_iterator.initializer)
 
   if mode == ModeKeys.EVAL:
     val_iterator = batches \
       .make_initializable_iterator()
     image_tensor, caption_tensor, cap_idx_tensor, cap_len_tensor = \
       val_iterator.get_next()
-    tf.add_to_collection("initializer", val_iterator.initializer)
+    tf.add_to_collection("val_initializer", val_iterator.initializer)
+    scaffold = tf.train.Scaffold(init_op=val_iterator.initializer)
 
-  vggnet = Vgg19(params.vgg_model_path)
-  vggnet.build(image_tensor)
+  _, end_points = vgg.vgg_16(image_tensor)
+  image_features = end_points['vgg_16/conv5/conv5_3']
+  image_features = tf.reshape(image_features, shape=[-1, 196, 512])
+
+  if mode == ModeKeys.TRAIN:
+    variables_to_restore = slim.get_variables_to_restore(exclude=['fc6', 'fc7', 'fc8'])
+    init_fn = assign_from_checkpoint_fn(params.vgg_model_path, variables_to_restore)
+    # signature of sc
+    scaffold = tf.train.Scaffold(init_fn=lambda _, sess: init_fn(sess))
 
   model = AttendTell(vocab_size=params.vocab_size)
-  outputs = model.build(vggnet.features, cap_idx_tensor)
+  outputs = model.build(image_features, cap_idx_tensor)
   predictions = tf.argmax(outputs, axis=-1)
   loss_op = None
   train_op = None
@@ -57,7 +69,8 @@ def model_fn(features, labels, mode, params, config):
     mode=mode,
     predictions=predictions,
     loss=loss_op,
-    train_op=train_op
+    train_op=train_op,
+    scaffold=scaffold
     # eval_metric_ops={"Accuracy": tf.metrics.accuracy(labels=labels['answer'], predictions=predictions, name='accuracy')}
   )
 
@@ -126,7 +139,8 @@ def experiment_fn(run_config, hparams):
     params=hparams,
     config=run_config)
 
-  initializer_hook = IteratorInitializerHook()
+  train_init_hook = IteratorInitializerHook("train")
+  val_init_hook = IteratorInitializerHook("val")
 
   experiment = Experiment(
     estimator=estimator,
@@ -135,9 +149,9 @@ def experiment_fn(run_config, hparams):
     train_steps=hparams.train_steps,
     eval_steps=500,
     train_steps_per_iteration=hparams.steps_per_eval,
-    eval_hooks=[initializer_hook],
+    eval_hooks=[val_init_hook],
   )
-  experiment.extend_train_hooks([initializer_hook])
+  experiment.extend_train_hooks([train_init_hook])
   return experiment
 
 
@@ -158,7 +172,7 @@ def get_parser():
   parser.add_argument("--batch-size", dest="batch_size", type=int, default=2,
                       help="Batch size.")
   parser.add_argument("--vgg-model-path", dest="vgg_model_path", type=str,
-                      default="data/imagenet-vgg-verydeep-19.mat",
+                      default="data/model/vgg_16.ckpt",
                       help="Path of pre-trained VGG parameters.")
   return parser
 
@@ -190,75 +204,9 @@ def main():
   learn_runner.run(
     experiment_fn=experiment_fn,
     run_config=run_config,
-    schedule="continuous_train_and_eval",
+    schedule="train",
     hparams=params
   )
-
-
-def old_main():
-  with open("data/annotations/captions_train2014.json") as f:
-    annotations = json.load(f)
-
-  id_to_filename = {img['id']: img['file_name'] for img in annotations['images']}
-
-  # todo: to achieve totally end-to-end training, we should
-  #   call `lower` in tensor-style. But TF do not support
-  #   `string_lower` directly right now.
-  cap_fn_pairs = [(process_caption(ann['caption']), os.path.join("image/train", id_to_filename[ann['image_id']]))
-                  for ann in annotations['annotations']]
-
-  captions, filenames = list(zip(*cap_fn_pairs))
-
-  caption_dataset = Dataset.from_tensor_slices(list(captions))
-  filename_dataset = Dataset.from_tensor_slices(list(filenames))
-
-  with open(os.path.join("data", 'word_to_idx.pkl'), 'rb') as f:
-    word_to_idx = pickle.load(f)
-
-  table = HashTable(KeyValueTensorInitializer(list(word_to_idx.keys()), list(word_to_idx.values()),
-                                              key_dtype=tf.string, value_dtype=tf.int32),
-                    0)
-
-  def split_sentence(sentence):
-    words = tf.string_split(tf.reshape(sentence, (1,))).values
-    words = tf.concat([tf.constant(["<START>"]), words, tf.constant(["<END>"])], axis=0)
-    return table.lookup(words)
-
-  index_dataset = caption_dataset.map(split_sentence)
-
-  caption_length_dataset = index_dataset.map(lambda t: tf.size(t))
-
-  def decode_image(filename):
-    image = tf.image.decode_jpeg(tf.read_file(filename))
-    image = tf.image.resize_images(image, [224, 224])
-    image = tf.to_float(image)
-    return image
-
-  image_dataset = filename_dataset.map(decode_image)
-
-  iterator = Dataset.zip((image_dataset, caption_dataset, index_dataset, caption_length_dataset)).padded_batch(2, (
-    (224, 224, 3), (), (None,), ())).make_initializable_iterator()
-  image_tensor, caption_tensor, index_tensor, length_tensor = iterator.get_next()
-  vgg_model_path = "/Users/dtong/code/ai/ImSAT/data/imagenet-vgg-verydeep-19.mat"
-  vggnet = Vgg19(vgg_model_path)
-  vggnet.build(image_tensor)
-  model = AttendTell(len(word_to_idx))
-  outputs = model.build(features=vggnet.features, captions=index_tensor)
-  loss_op = create_loss(outputs, index_tensor, length_tensor)
-
-  optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-  trainables = tf.trainable_variables()
-  grads = optimizer.compute_gradients(loss_op, trainables)
-  global_step = tf.contrib.framework.get_global_step()
-  train_op = optimizer.apply_gradients(grads, global_step=global_step)
-
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
-  sess.run(table.init)
-  sess.run(iterator.initializer)
-  for i in range(30):
-    loss, _ = sess.run([loss_op, train_op])
-    print(loss)
 
 
 def process_caption(caption):
