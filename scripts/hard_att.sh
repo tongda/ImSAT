e1af579b4a22a386df1d012029b3f4a14f12d409
Tong Damon Da <tongda@outlook.com>
2017-10-10 22:38:09 +0800

    fix issue in baseline RL.

--- scripts/hard_att.sh
+++ scripts/hard_att.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python3 train.py --train-steps 30000 --steps-per-eval 5000 --batch-size 8 --learning-rate 0.0001 --selector --dropout --ctx2out --prev2out --dataset challenger.ai --eval-steps 500 --hard-attention
+python3 train.py --train-steps 30000 --steps-per-eval 5000 --batch-size 8 --learning-rate 0.0001 --selector --dropout --ctx2out --prev2out --dataset challenger.ai --eval-steps 200 --hard-attention
 
-python3 train.py --train-steps 60000 --steps-per-eval 1000 --batch-size 8 --learning-rate 0.0001 --selector --dropout --ctx2out --prev2out --dataset challenger.ai --eval-steps 500 --hard-attention --use-sampler
+python3 train.py --train-steps 100000 --steps-per-eval 1000 --batch-size 8 --learning-rate 0.001 --selector --dropout --ctx2out --prev2out --dataset challenger.ai --eval-steps 200 --hard-attention --use-sampler
